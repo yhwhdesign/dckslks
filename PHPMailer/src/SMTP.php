89197833d703cd3c3dd35da15f2905404261f5a6
jburkett <yhwhdesign@gmail.com>
2023-02-01 17:40:23 -0600

    updating phpMailer files

--- PHPMailer/src/SMTP.php
+++ PHPMailer/src/SMTP.php
@@ -35,7 +35,7 @@ class SMTP
      *
      * @var string
      */
-    const VERSION = '6.6.3';
+    const VERSION = '6.7.1';
 
     /**
      * SMTP line break constant.
@@ -49,7 +49,7 @@ class SMTP
      *
      * @var int
      */
-    const DEFAULT_PORT = 456;
+    const DEFAULT_PORT = 25;
 
     /**
      * The maximum line length allowed by RFC 5321 section 4.5.3.1.6,
@@ -187,6 +187,7 @@ class SMTP
         'SendGrid' => '/[\d]{3} Ok: queued as (.*)/',
         'CampaignMonitor' => '/[\d]{3} 2.0.0 OK:([a-zA-Z\d]{48})/',
         'Haraka' => '/[\d]{3} Message Queued \((.*)\)/',
+        'ZoneMTA' => '/[\d]{3} Message queued as (.*)/',
         'Mailjet' => '/[\d]{3} OK queued as (.*)/',
     ];
 
@@ -682,7 +683,6 @@ class SMTP
      */
     public function close()
     {
-        $this->setError('');
         $this->server_caps = null;
         $this->helo_rply = null;
         if (is_resource($this->smtp_conn)) {
